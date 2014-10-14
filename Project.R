b63f565731a706d8a7eab60f115ee8975fd18746
throbinson <todd@biarchitect.com>
2014-10-14 08:21:54 -0500

    test

--- Project.R
+++ Project.R
@@ -3,3 +3,4 @@ load(url("http://bit.ly/dasi_gss_data"))
 download.file(url = "http://bit.ly/dasi_project_template", destfile = "dasi_project_template.Rmd")
 source("http://bit.ly/dasi_inference")
 
+#ffff
